27dcecaa75b27c0ce5bb10af2a032b2de4b78fa6
Ian Sodersjerna <iansodersjerna@gmail.com>
2021-11-19 13:01:16 -0500

    sled and car prefab and attachment script
    added sled and car prefabs and a very very basic attachment script

---
+++ Assets/Scripts/Attachment.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attachment : MonoBehaviour
+{
+    public GameObject ControlledObject;
+
+    public bool Ice_sleds_Enabled = false;
+    public bool Jet_Enabled = false;
+    public bool Helicopter_Enabled = false;
+
+    public static int starting_fuel = 100;
+    public float current_fuel = starting_fuel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
