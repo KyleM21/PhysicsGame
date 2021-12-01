01e72bdc5e59e5e89446f8f97a58a61dd27453f6
Mit Bailey <mitbailey99@gmail.com>
2021-12-01 00:15:58 -0500

    Added TODO list to README, made the victory goal pause the game and display the victory menu allowing player to continue to next level

--- Assets/Scripts/GoalHitDetect.cs
+++ Assets/Scripts/GoalHitDetect.cs
@@ -1,25 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoalHitDetect : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject VictoryMenu;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        VictoryMenu.SetActive(false);
     }
 	
 	void OnTriggerEnter2D(Collider2D Object){
 		if(Object.gameObject.tag == "Player"){
 			Debug.Log("Goal Met");
+            VictoryMenuHandler();
 		}
 	}
-		
+
+    void VictoryMenuHandler()
+    {
+        Time.timeScale = 0f;
+        VictoryMenu.SetActive(true);
+    }
+
+    public void NextLevel()
+    {
+        Time.timeScale = 1;
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "LevelOne":
+            {
+                SceneManager.LoadScene("LevelTwo");
+                break;
+            }
+            case "LevelTwo":
+            {
+                SceneManager.LoadScene("LevelThree");
+                break;
+            }
+            default:
+            {
+                SceneManager.LoadScene("MainMenu");
+                break;
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        VictoryMenu.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitToDesktop()
+    {
+        Application.Quit();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Please view the 'release' branch for information on running the game.
 Bugfixes:
 1. Was launching into game, not menu
 2. Added a start level button to remove confusion
+
+To-do:
+
+- Make goal end the level
+
+- Add sound effects
+
+- Reduce speed at which FunSpheres flash.
